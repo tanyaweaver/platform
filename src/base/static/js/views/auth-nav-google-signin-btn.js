ec2d9c1c45abcdd6fa8173924a03b94b8ca36926
Sung J Byun <sungjbyun@D-108-179-171-167.dhcp4.washington.edu>
2017-03-14 16:56:01 -0700

    working on OAuth2; redo credentials using Google+

---
+++ src/base/static/js/views/auth-nav-google-signin-btn.js
@@ -0,0 +1,14 @@
+  // var Util = require('../utils.js');
+  //
+  // module.exports = Backbone.View.extend({
+  //   onSignIn: function(googleUser) {
+  //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //     console.log('Name: ' + profile.getName());
+  //     console.log('Image URL: ' + profile.getImageUrl());
+  //     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  //
+  //     var id_token = googleUser.getAuthResponse().id_token;
+  //     console.log("ID Token: " + id_token);
+  //   },
+  //
+  // });
